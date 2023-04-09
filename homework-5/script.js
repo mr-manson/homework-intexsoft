b87f40d8789b2e2f044dc13cd858feeff922f657
mr-manson <nikola777jr@gmail.com>
2023-04-09 19:03:06 +0300

    add empty array check;

--- homework-5/script.js
+++ homework-5/script.js
@@ -1,12 +1,18 @@
 let arr = [1, 2, 3, 4, 50];
-notArray = 1;
+let notArray = 1;
+let empty = [];
 
+// ====CECK=FUNCTIONS=================
 let summ = function (a, b) {
   return a + b;
 };
 
-let less = function (a) {
+let less = function (a, b) {
   return a <= 10;
+};
+
+let isNumber = function (a) {
+  return typeof a === Number;
 };
 
 // ====ARRAY.EVERY====================
@@ -19,7 +25,7 @@ let res1 = arr.every(summ);
 // console.log(res1);
 
 // ===CUSTOM=FUNCTION=================
-let newEvery = function (arr, func) {
+let customEvery = function (arr, func) {
   let res = 0;
   for (let i = 0; i < arr.length; i++) {
     res = func(arr[i], i);
@@ -27,19 +33,29 @@ let newEvery = function (arr, func) {
   }
 };
 
-// newEvery(arr, summ);
+// customEvery(arr, summ);
 
 // ===CUSTOM=ARRAY.PROTOTYPE==========
 Array.prototype.customEvery = function (func) {
-  let res = 0;
-  for (let i = 0; i < this.length; i++) {
-    res = func(this[i], i, this);
-    console.log(res);
+  if (!Array.isArray(this)) {
+    throw Error(`${this} isn't an array`);
+  }
+  if (this.length === 0) {
+    return true;
+  } else {
+    let res = 0;
+    for (let i = 0; i < this.length; i++) {
+      res = func(this[i], i, this);
+      console.log(res);
+    }
   }
 };
 
-arr.customEvery(summ);
-arr.customEvery(less);
-//notArray.newEvery(less);
+// arr.customEvery(summ);
+// arr.customEvery(less);
+// arr.customEvery(isNumber);
+// console.log(empty.customEvery(less));
+
+// console.log(notArray.customEvery(summ));
 
 //console.log(Array.isArray(arr));
