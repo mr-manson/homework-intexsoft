8e842f6c2e30f771e69e8898ab280885744c83e6
mr-manson <nikola777jr@gmail.com>
2023-04-22 14:48:54 +0300

    var to const-let, add ? and ` ;

--- homework-7 - sms/script.js
+++ homework-7 - sms/script.js
@@ -1,7 +1,7 @@
 "use strict";
 
 addEventListener("DOMContentLoaded", function () {
-  var a =
+  const a =
     "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc, quis gravida magna mi a libero. Fusce vulputate eleifend sapien. Vestibulum purus quam, scelerisque ut, mollis sed, nonummy id, metus. Nullam accumsan lorem in dui. Cras ultricies mi eu turpis hendrerit fringilla. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In ac dui quis mi consectetuer lacinia. Nam pretium turpis et arcu. Duis arcu tortor, suscipit eget, imperdiet nec, imperdiet iaculis, ipsum. Sed aliquam ultrices mauris. Integer ante arcu, accumsan a, consectetuer eget, posuere ut, mauris. Praesent adipiscing. Phasellus ullamcorper ipsum rutrum nunc. Pellentesque libero tortor, tincidunt et, tincidunt eget, semper nec, quam. Sed hendrerit. Morbi ac felis.";
   console.log(getAllSMS(a)); // big text
   console.log(getAllSMS("Lorem ipsum dolor sit amet")); // small text
@@ -9,13 +9,16 @@ addEventListener("DOMContentLoaded", function () {
   console.log(getAllSMS({})); // wrong props
 });
 
+let splitText = "";
+let result = [];
+const initIter = 1;
+
 const getAllSMS = (inputText) => {
   if (!inputText || typeof inputText !== "string") return [];
-  var splitText = inputText.split(" ");
-  var initIter = 1;
+  splitText = inputText.split(" ");
 
-  for (var i = 1; i <= 4; i++) {
-    var result = getSMS(splitText, initIter, i);
+  for (let i = 1; i <= 4; i++) {
+    result = getSMS(splitText, initIter, i);
     if (!result.needNewRecursion) break;
   }
   if (result && result.needNewRecursion) {
@@ -27,37 +30,37 @@ const getAllSMS = (inputText) => {
   }
 };
 
-var totalIterationCount;
+let totalIterationCount = 0;
+let iteration = 0;
+let iterationEndSize = 0;
 
 const getSMS = (arr, it = 1, itEndSize = 1) => {
-  var iteration = it;
-  var iterationEndSize = itEndSize;
+  iteration = it;
+  iterationEndSize = itEndSize;
 
   if (!arr.length) {
-    totalIterationCount = iteration - 1;
+    totalIterationCount = iteration--;
     return { result: arr };
   }
 
-  if (iterationEndSize < String(iteration).length) {
-    return { result: [], needNewRecursion: true };
-  }
+  iterationEndSize < String(iteration).length
+    ? { result: [], needNewRecursion: true }
+    : "";
 
-  var smsText = "";
-  var count = 0;
+  let smsText = "";
+  let count = 0;
+  let smsTextOne = `${smsText}${arr[count]} ${iteration}/`;
+
   while (smsText.length < 140 && count < arr.length) {
-    if (
-      (smsText + arr[count] + " " + iteration + "/").length +
-        iterationEndSize <=
-      140
-    ) {
+    if (smsTextOne.length + iterationEndSize <= 140) {
       smsText += arr[count] + " ";
       count++;
     } else break;
   }
   smsText += iteration + "/";
 
-  var iterationResult = getSMS(arr.slice(count), ++iteration, iterationEndSize);
-  var result = [smsText].concat(iterationResult.result);
+  let iterationResult = getSMS(arr.slice(count), ++iteration, iterationEndSize);
+  let result = [smsText].concat(iterationResult.result);
   if (result[0]) result[0] += totalIterationCount;
-  return { result: result, needNewRecursion: iterationResult.needNewRecursion };
+  return { result, needNewRecursion: iterationResult.needNewRecursion };
 };
